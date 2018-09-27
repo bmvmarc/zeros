7c0010dbf270f2869498ef60cfe5f2ee9e4d830a
Marina Chichel <bmvmarc@gmail.com>
2018-09-27 15:10:33 +0300

    zeros implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+    let sum = 0, divider = 5;
+
+    while (true) {
+
+        quantity = (number - number % divider) / divider;
+
+        if (quantity > 0) {
+            sum += quantity;
+            divider *= 5;
+        }
+        else {
+            break;
+        }
+    }
+
+    return sum;
+
 }
